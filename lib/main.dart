a8e0ac2813efcab4b4b4d042712fae99090a642e
Surya Dev Singh <46717624+suryadevsingh@users.noreply.github.com>
2019-10-21 20:59:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ashish Rawat',
+      title: "Ashish Rawat",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "GoogleSans"),
       home: Home(),
